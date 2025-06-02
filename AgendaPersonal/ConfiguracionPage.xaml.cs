c46a267564716fe701943332c57a44c897e6fd55
EricC <al23760307@ite.edu.mx>
2025-06-01 19:03:29 -0700

    configuracion de tema

--- AgendaPersonal/ConfiguracionPage.xaml.cs
+++ AgendaPersonal/ConfiguracionPage.xaml.cs
@@ -6,4 +6,22 @@ public partial class ConfiguracionPage : ContentPage
     {
         InitializeComponent();
     }
+    private void OnThemeToggleClicked(object sender, EventArgs e)
+    {
+        // Obtén el tema actual
+        var currentTheme = Application.Current.UserAppTheme;
+
+        // Cambia al otro tema
+        if (currentTheme == AppTheme.Dark)
+        {
+            Application.Current.UserAppTheme = AppTheme.Light;
+            
+        }
+        else
+        {
+            Application.Current.UserAppTheme = AppTheme.Dark;
+            
+        }
+    }
 }
+
